dad450c98a6ac9ec06ff072a27145fff4ffedebf
arshiya <arshiya.ux@gmail.com>
2020-07-21 11:35:10 +0530

    create pages
    clearly create main about and fashon page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,14 +4,4 @@ $(document).ready(function() {
   });
 });
 
-// Scrolling Effect
 
-// $(window).on("scroll", function() {
-//   if($(window).scrollTop()) {
-//         $('nav').addClass('black');
-//   }
-
-//   else {
-//         $('nav').removeClass('black');
-//   }
-// })
